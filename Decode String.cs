59b5dac32d48d976dc861e2f19c414d266ba73a2
Euaell <euaelmeko@gmail.com>
2022-04-18 16:52:36 +0300

    undone indentesion

--- Decode String.cs
+++ Decode String.cs
@@ -1,38 +1,38 @@
 public class Solution {
     public string DecodeString(string s) {
         string x = "";
-            for (int i = 0; i < s.Length; i++)
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (Char.IsDigit(s[i]))
             {
-                if (Char.IsDigit(s[i]))
+                string inp = "";
+                while (s[i] != '[')
                 {
-                    string inp = "";
-                    while (s[i] != '[')
-                    {
-                        inp = inp.Insert(inp.Length, s[i].ToString());
-                        i++;
-                    }
+                    inp = inp.Insert(inp.Length, s[i].ToString());
                     i++;
-                    int cou = 0;
-                    int pass = 0;
-                    for (int j = i; j < s.Length; j++)
-                    {
-                        if (s[j] == '[')
-                            cou--;
-                        else if (s[j] == ']')
-                            cou++;
-                        if (cou > 0 || j == s.Length - 1)
-                            break;
+                }
+                i++;
+                int cou = 0;
+                int pass = 0;
+                for (int j = i; j < s.Length; j++)
+                {
+                    if (s[j] == '[')
+                        cou--;
+                    else if (s[j] == ']')
+                        cou++;
+                    if (cou > 0 || j == s.Length - 1)
+                        break;
 
-                        pass++;
-                    }
-                    string temp = String.Concat(Enumerable.Repeat(DecodeString(s.Substring(i, pass)), Int32.Parse(inp)));
-                    i += pass;
-                    x = x.Insert(x.Length, temp);
-                } 
-                else if (Char.IsLetter(s[i]))
-                    x = x.Insert(x.Length, s[i].ToString());
-            }
+                    pass++;
+                }
+                string temp = String.Concat(Enumerable.Repeat(DecodeString(s.Substring(i, pass)), Int32.Parse(inp)));
+                i += pass;
+                x = x.Insert(x.Length, temp);
+            } 
+            else if (Char.IsLetter(s[i]))
+                x = x.Insert(x.Length, s[i].ToString());
+        }
             
-            return x;
+        return x;
     }
 }
